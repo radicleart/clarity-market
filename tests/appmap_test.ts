17122a151bb9bb23b05022943c8d8d97122cad42
Daniel Zhou <daniel.zhou2012@gmail.com>
2021-06-03 13:04:58 -0400

    administrator, changing app status, retrieving apps from map test cases

--- tests/appmap_test.ts
+++ tests/appmap_test.ts
@@ -8,6 +8,7 @@ import {
 import {
   assertEquals,
   assertStringIncludes,
+  assertArrayIncludes,
 } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 const formatBuffString = (buffer: string) => {
@@ -17,12 +18,13 @@ const formatBuffString = (buffer: string) => {
 Clarinet.test({
   name: "Project map tests",
   async fn(chain: Chain, accounts: Map<string, Account>) {
+    // set up
     const deployer = accounts.get("deployer")!;
     const wallet_1 = accounts.get("wallet_1")!;
     const wallet_2 = accounts.get("wallet_2")!;
-
     const asset_map = chain.getAssetsMaps();
 
+    // should return admin address and 0 app-counter
     let block = chain.mineBlock([
       Tx.contractCall("appmap", "get-administrator", [], deployer.address),
       Tx.contractCall("appmap", "get-app-counter", [], deployer.address),
@@ -43,12 +45,12 @@ Clarinet.test({
       "should return initial value of 0 for app-counter (indicates zero apps)"
     );
 
+    // should fail insert new application if illegal storage
     block = chain.mineBlock([
       Tx.contractCall(
         "appmap",
         "register-app",
         [
-          // unable to make this mijoco.id.blockstack
           types.principal(wallet_1.address),
           types.buff(formatBuffString(`${wallet_1.address}.my-project`)),
           types.int(50),
@@ -59,33 +61,255 @@ Clarinet.test({
 
     assertEquals(block.receipts.length, 1);
     assertEquals(block.height, 3);
-    // assertStringIncludes(
-    //   block.receipts[0].result,
-    //   "(err u102)",
-    //   "should fail insert new application if illegal storage"
-    // );
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(err u102)",
+      "should fail insert new application if illegal storage"
+    );
 
-    // block = chain.mineBlock([
-    //   Tx.contractCall(
-    //     "appmap",
-    //     "register-app",
-    //     [
-    //       // unable to make this mijoco.id.blockstack
-    //       types.principal(
-    //         "mijoco.id.blockstackmijoco.id.blockstackmijoco.id.blockstackmijoco.id.blockstackmijoco.id.blockstackijoco.id.blockstackmijoco.id.blockstack"
-    //       ),
-    //       types.buff(formatBuffString(`${wallet_1.address}.my-project`)),
-    //       types.int(50),
-    //     ],
-    //     deployer.address
-    //   ),
-    // ]);
-    // assertEquals(block.receipts.length, 1);
-    // assertEquals(block.height, 4);
-    // assertStringIncludes(
-    //   block.receipts[0].result,
-    //   "(err u102)",
-    //   "should fail insert new application if owner more than 80 chars"
-    // );
+    // should allow insert 2 new applications
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "appmap",
+        "register-app",
+        [
+          types.principal(wallet_1.address),
+          types.buff(formatBuffString(`${wallet_1.address}.my-gaia-project`)),
+          types.int(1),
+        ],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "appmap",
+        "register-app",
+        [
+          types.principal(wallet_1.address),
+          types.buff(
+            formatBuffString(`${wallet_1.address}.my-filecoin-project`)
+          ),
+          types.int(2),
+        ],
+        wallet_1.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.height, 4);
+    assertArrayIncludes(
+      block.receipts,
+      [
+        {
+          events: [
+            {
+              contract_event: {
+                contract_identifier:
+                  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.appmap",
+                topic: "print",
+                value: "1",
+              },
+              type: "contract_event",
+            },
+          ],
+          result: "(ok 1)",
+        },
+        {
+          events: [
+            {
+              contract_event: {
+                contract_identifier:
+                  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.appmap",
+                topic: "print",
+                value: "2",
+              },
+              type: "contract_event",
+            },
+          ],
+          result: "(ok 2)",
+        },
+      ],
+      "should allow insert 2 new applications"
+    );
+
+    // should indicate 2 apps
+    block = chain.mineBlock([
+      Tx.contractCall("appmap", "get-app-counter", [], deployer.address),
+    ]);
+
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 5);
+
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok 2)",
+      "should indicate 2 apps"
+    );
+
+    // allow insert another 2 applications
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "appmap",
+        "register-app",
+        [
+          types.principal(wallet_1.address),
+          types.buff(formatBuffString(`${wallet_1.address}.my-upfs-project`)),
+          types.int(5),
+        ],
+        deployer.address
+      ),
+      Tx.contractCall(
+        "appmap",
+        "register-app",
+        [
+          types.principal(wallet_1.address),
+          types.buff(formatBuffString(`${wallet_1.address}.my-storj-project`)),
+          types.int(6),
+        ],
+        deployer.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.height, 6);
+    assertArrayIncludes(
+      block.receipts,
+      [
+        {
+          events: [
+            {
+              contract_event: {
+                contract_identifier:
+                  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.appmap",
+                topic: "print",
+                value: "3",
+              },
+              type: "contract_event",
+            },
+          ],
+          result: "(ok 3)",
+        },
+        {
+          events: [
+            {
+              contract_event: {
+                contract_identifier:
+                  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.appmap",
+                topic: "print",
+                value: "4",
+              },
+              type: "contract_event",
+            },
+          ],
+          result: "(ok 4)",
+        },
+      ],
+      "should allow 2 new applications"
+    );
+
+    // should indicate 4 apps
+    block = chain.mineBlock([
+      Tx.contractCall("appmap", "get-app-counter", [], deployer.address),
+    ]);
+
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 7);
+
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok 4)",
+      "should indicate 4 apps"
+    );
+
+    // should be able to get apps from map
+    block = chain.mineBlock([
+      Tx.contractCall("appmap", "get-app", [types.int(0)], deployer.address),
+      Tx.contractCall("appmap", "get-app", [types.int(1)], deployer.address),
+    ]);
+
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.height, 8);
+
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok {app-contract-id: 0x5354314a34473652523634334243473847385352364d3244395a394b5854324e4a44524b334642544b2e6d792d676169612d70726f6a656374, owner: ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK, status: 0, storage-model: 1})",
+      "should be able to get app 1 from map"
+    );
+    assertStringIncludes(
+      block.receipts[1].result,
+      "(ok {app-contract-id: 0x5354314a34473652523634334243473847385352364d3244395a394b5854324e4a44524b334642544b2e6d792d66696c65636f696e2d70726f6a656374, owner: ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK, status: 0, storage-model: 2})",
+      "should be able to get app 2 from map"
+    );
+
+    // should not be able to set apps to live if not administrator or not contract owner
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "appmap",
+        "set-app-status",
+        [types.int(0), types.int(1)],
+        wallet_2.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 9);
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(err u101)",
+      "should not be able to set apps to live if not administrator or not contract owner"
+    );
+
+    // should be able to set apps to live if contract owner
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "appmap",
+        "set-app-status",
+        [types.int(0), types.int(1)],
+        wallet_1.address
+      ),
+    ]);
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok true)",
+      "should be able to set apps to live if contract owner"
+    );
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 10);
+
+    // should be able to check status of app set to live
+    block = chain.mineBlock([
+      Tx.contractCall("appmap", "get-app", [types.int(0)], deployer.address),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 11);
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok {app-contract-id: 0x5354314a34473652523634334243473847385352364d3244395a394b5854324e4a44524b334642544b2e6d792d676169612d70726f6a656374, owner: ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK, status: 1, storage-model: 1})",
+      "should be able to check app 1 status set to live"
+    );
+
+    // should be able to set apps to live if administrator
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "appmap",
+        "set-app-status",
+        [types.int(1), types.int(1)],
+        deployer.address
+      ),
+    ]);
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok true)",
+      "should be able to set apps to live if contract owner"
+    );
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 12);
+
+    // should be able to check status of app set to live
+    block = chain.mineBlock([
+      Tx.contractCall("appmap", "get-app", [types.int(1)], deployer.address),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 13);
+    assertStringIncludes(
+      block.receipts[0].result,
+      "(ok {app-contract-id: 0x5354314a34473652523634334243473847385352364d3244395a394b5854324e4a44524b334642544b2e6d792d66696c65636f696e2d70726f6a656374, owner: ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK, status: 1, storage-model: 2})",
+      "should be able to check app 2 status set to live"
+    );
   },
 });
